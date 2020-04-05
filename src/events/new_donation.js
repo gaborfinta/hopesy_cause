045a16d759f9b436e22944b004cb8738576ca3c4
nagybalint <nagybalint000@gmail.com>
2020-04-05 07:00:59 +0200

    Finish new donation event listener

--- src/events/new_donation.js
+++ src/events/new_donation.js
@@ -1,16 +1,16 @@
 const admin = require('firebase-admin');
 
 async function onNewDonation(snap, context) {
-    console.log(snap.data());
-    const { id, amount, cause_id } = snap.data();
+    console.log(context);
+    const { id } = context.params;
+    const { amount, cause_id } = snap.data();
     let causeReference = await admin.firestore().collection('causes').doc(cause_id);
     let causeRecord = await causeReference.get();
     let causeData = causeRecord.data();
-    console.log(causeData);
-    causeData.donations.push(id);
-    causeData.sum_collected += amount;
-    if (sum_collected >= causeData.sum_target) {
-        causeData.closed = true;
+    causeData["donations"].push(id);
+    causeData["sum_collected"] += amount;
+    if (causeData["sum_collected"] >= causeData["sum_target"]) {
+        causeData["closed"] = true;
     }
     await causeReference.set(causeData);
 }
